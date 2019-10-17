cdaeffc293fd0bf333d7048e2cb587d977b90a05
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-10-17 11:45:15 -0400

    feat: Initial commit, master.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Package Name
+
+## Description
+
+Package description.
+
+## Installation
+
+```
+pip install package_name
+```
+
+## Usage
+
+Usage here.
---
+++ src/rest_framework_expandable/mixins/expandable.py
@@ -0,0 +1,81 @@
+from ..utils import sort_field_paths
+
+
+class ExpandableMixin(object):
+    model_name = None
+    query_param = "expand"
+    target_fields = None
+
+    @property
+    def request(self):
+        """
+        Returns the current request context passed from DRF.
+        """
+        context = getattr(self, "context", None)
+        if context is None:
+            raise AttributeError("Context not found.")
+
+        request = context.get("request", None)
+        if request is None:
+            raise AttributeError("Request not found in context.")
+
+        return request
+
+    @property
+    def params(self):
+        """
+        Returns a list of unique relative field paths that should be used for expanding.
+        """
+        field_paths = []
+
+        attr_name = getattr(self, "query_param", None)
+        if attr_name is not None:
+            query_params = getattr(self.request, "query_params", {})
+            query_params = query_params.get(attr_name, "").split(",")
+            for param in query_params:
+                field_paths.append(param)
+
+        return sort_field_paths(field_paths)
+
+    def get_model_name(self):
+        """
+        Returns the model name from the ModelSerializer Meta class model specified, or
+        from the previously saved model name on the class.
+        """
+        model_name = getattr(self, "model_name", None)
+
+        if model_name is None:
+            model = self.Meta.model
+            model_name = model.__name__.lower()
+            self.model_name = model_name
+
+        return model_name
+
+    def get_field_path(self, path):
+        """
+        Returns a list of possible field paths that are prefixed with the current
+        serializers model name, plus one suffixed with _set for django's default
+        reverse relationship names.
+        """
+        model_name = self.get_model_name()
+        prefix = "{}.".format(model_name)
+        if not path.startswith(prefix):
+            return "{}{}".format(prefix, path)
+        return path
+
+    @property
+    def requested_fields(self):
+        """
+        Returns a list of field paths to expand.
+        Can be specified via class instance or via query params.
+        """
+        requested_fields = self.params
+
+        # Add our target fields that we specified on the class.
+        if isinstance(self.target_fields, list):
+            for target_field in self.target_fields:
+                requested_fields.append(target_field)
+
+        requested_fields = sort_field_paths(requested_fields)
+        print(requested_fields)
+        return requested_fields
